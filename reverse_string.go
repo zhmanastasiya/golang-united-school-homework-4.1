6b08e6ea086f2272b827838915dc7bf243d295a1
zhmanastasiya <zhmanastasiya@mail.ru>
2022-05-17 12:42:10 +0400

    reverse hello world

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
 }
